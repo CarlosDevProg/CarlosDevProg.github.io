ff35ab0eef986521fd4146b330ea6b1c5eeea0bd
Carlos Salvador <carlos1992.ces@gmail.com>
2020-03-28 23:58:10 -0300

    Modificação no sender 1.0.2

--- php/sender.php
+++ php/sender.php
@@ -1,24 +1,33 @@
 <?php
 
-// isset checa se o botão enviar foi clicado e só vai disparar o email se for verdadeiro
+// Destinatário
+$para = "carlos1992.ces@gmail.com";
 
-if (isset($_POST['enviar'])) {
-    $msg = "Nome: " . $_POST["name"] . "<br />";
-    $msg .= "Email: " . $_POST["email"] . "<br />";
-    $msg .= "Mensagem:<br />" . $_POST["message"];
-    
-    // email que receberá a mensagem
-    $destinatario = "carlos1992.ces@gmail.com";
+// Assunto do e-mail (Para futuras implementações)
+//$assunto = "Contato do através do site ...";
 
-    // headers que preparam a mensagem
-    $headers = "MIME-version: 1.0\r\n";
-    $headers .= "Content-type: text/html; charset=utf-8\r\n";
-    $headers .= "From: " . $_POST["name"] . "<" . $_POST["email"] . ">\r\n";
-    $headers .= "Reply-To: " . $_POST["email"] . "\r\n";
+// Campos do formulário de contato
+$nome = $_POST['name'];
+$email = $_POST['email'];
+// $telefone = $_POST['tel'];(Para futuras implementações)
+$mensagem = $_POST['content'];
 
-    // envia o email... 
-    mail($destinatario,$msg,$headers);
+// Monta o corpo da mensagem com os campos
+$corpo = "<html><body>";
+$corpo .= "Nome: $nome ";
+$corpo .= "Email: $email Mensagem: $mensagem";
+$corpo .= "</body></html>";
 
-    // volta para contato.html
-    header("Location: contato.html");
+// Cabeçalho do e-mail
+$email_headers = implode("\n", array("From: $nome", "Reply-To: $email", "Return-Path: $email", "MIME-Version: 1.0", "X-Priority: 3", "Content-Type: text/html; charset=UTF-8"));
+
+//Verifica se os campos estão preenchidos para enviar então o email
+if (!empty($nome) && !empty($email) && !empty($mensagem)) {
+    mail($para, $corpo, $email_headers);
+    $msg = "Sua mensagem foi enviada com sucesso.";
+    echo "<script>alert('$msg');window.location.assign('https://curriculocarlos.000webhostapp.com/contato.html');</script>";
+} else {
+    $msg = "Erro ao enviar a mensagem.";
+    echo "<script>alert('$msg');window.location.assign('https://curriculocarlos.000webhostapp.com/contato.html');</script>";
 }
+?>
