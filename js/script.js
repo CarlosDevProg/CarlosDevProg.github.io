87072bbd2060f4c2d3f8c7adf624c0d465cb21ba
Carlos Salvador <carlos1992.ces@gmail.com>
2020-03-29 03:55:36 -0300

    Melhorias no design e sender 1.0.3

--- js/script.js
+++ js/script.js
@@ -3,6 +3,6 @@ function enviar() {
     var name = document.getElementById("nameid");
 
     if (name.value != "") {
-        alert('Obrigado pelo contato, ' + name.value +'. A sua mensagem foi enviada com sucesso!');
+        alert('Obrigado pelo contato, ' + name.value);
     }
 }
